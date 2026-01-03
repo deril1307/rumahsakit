206cc2619128c3bdf92652d5db81776f7815dee0
Deril Wijdan Falih <derilwijdan346@gmail.com>
2026-01-03 12:14:59 +0700

    memperbaiki tampilan nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -194,16 +194,17 @@
 
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
+                            {{-- MODIFIKASI: Ubah Text jadi Keluar & Warna Merah --}}
                             <x-dropdown-link :href="route('logout')"
-                                onclick="event.preventDefault();
-                                                this.closest('form').submit();"
-                                class="flex items-center gap-2">
+                                onclick="event.preventDefault(); this.closest('form').submit();"
+                                class="flex items-center gap-2 text-red-600 hover:text-red-800 hover:bg-red-50">
+                                
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-500">
+                                    stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-red-600">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                                 </svg>
-                                {{ __('Log Out') }}
+                                {{ __('Keluar') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -287,10 +288,11 @@
 
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
+                    {{-- MODIFIKASI MOBILE: Ubah Text jadi Keluar & Warna Merah --}}
                     <x-responsive-nav-link :href="route('logout')"
-                        onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        onclick="event.preventDefault(); this.closest('form').submit();"
+                        class="text-red-600 hover:text-red-800 hover:bg-red-50">
+                        {{ __('Keluar') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
